24a3475011c037a7e72774d1cfe0d519965978b3
Manel Clos <llanero@users.sourceforge.net>
2002-06-16 21:45:57 +0000

    remove old dirs and unpack current versions.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,13 @@
 #! /bin/sh
-# Run this to generate the configure script
+# Run this to generate the configure script and unpack needed packages
 
+# This generates the configure script from configure.in
 autoconf
+
+# This deletes the (old) pccts dir and unpacks the latest version
+rm -rf pccts
+tar xvzf pccts.tar.gz
+
+# This deletes the (old) scsi dir and unpacks the latest version
+rm -rf scsilib
+tar xvzf scsilib.tar.gz
