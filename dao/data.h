5a8311ef1bd4b4ce6f72cb5e19aee2ee8dcdd590
Andreas Mueller <andreasm@users.sourceforge.net>
2002-07-03 05:30:57 +0000

    Container for data.

---
+++ dao/data.h
@@ -0,0 +1,25 @@
+/*  cdrdao - write audio CD-Rs in disc-at-once mode
+ *
+ *  Copyright (C) 1998-2002 Andreas Mueller <andreas@daneb.de>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef __DATA_H__
+#define __DATA_H__
+
+extern unsigned char SECTOR_ERROR_DATA[2448];
+
+#endif
