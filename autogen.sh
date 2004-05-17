43d76cae4ccfe4317698053847577d4a483f044e
Denis Leroy <denis@poolshark.org>
2004-05-17 21:48:30 +0000

    Added automake version check. Also wipes out autom4te.cache

--- autogen.sh
+++ autogen.sh
@@ -15,6 +15,24 @@ if test -e scsilib.tar.gz ; then
   tar xzf scsilib.tar.gz
 fi
 
+# Minimum version
+MAJOR_VERSION=1
+MINOR_VERSION=6
+
+AMVERSION=`automake --version  | awk '/automake/ {print $4}'`
+
+if test `echo $AMVERSION | awk -F. '{print $1}'` -lt $MAJOR_VERSION; 
+then
+  echo "Your version of automake is too old, you need at least automake $MAJOR_VERSION.$MINOR_VERSION"
+  exit -1
+fi
+
+if test `echo $AMVERSION | awk -F. '{print $2}' | awk -F- '{print $1}'` -lt $MINOR_VERSION; then
+  echo "Your version of automake is too old, you need at least automake $MAJOR_VERSION.$MINOR_VERSION"
+  exit -1
+fi
+
 # Calls aclocal, automake, autoconf and al. for you
 echo "Running autoreconf"
+rm -fr autom4te.cache
 autoreconf
