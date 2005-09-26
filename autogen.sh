c0cdc79155f99fa6c7126b23dc222582305e5684
Denis Leroy <denis@poolshark.org>
2005-09-26 21:57:27 +0000

    Moved pccts into automake system rather than alien tarball

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,6 @@
 #! /bin/sh
 # Run this to generate the configure script and unpack needed packages
 
-# This deletes the (old) pccts dir and unpacks the latest version
-if test -e pccts.tar.gz ; then
-  rm -rf pccts
-  echo "Unpacking pccts.tar.gz"
-  tar xzf pccts.tar.gz
-fi
-
 # This deletes the (old) scsilib dir and unpacks the latest version
 if test -e scsilib.tar.gz ; then
   rm -rf scsilib
