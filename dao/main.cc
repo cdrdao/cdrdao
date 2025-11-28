fda39eef93c1fb10328541ba58312a4d601834ff
Denis Leroy <denis@soundhound.com>
2025-11-28 11:37:55 +0100

    Remove obsolete copyright message

--- dao/main.cc
+++ dao/main.cc
@@ -265,8 +265,7 @@ char devstr[10];
 
 void printVersion()
 {
-  log_message(2, "Cdrdao version %s - (C) Andreas Mueller <andreas@daneb.de>",
-	  VERSION);
+  log_message(2, "Cdrdao version %s", VERSION);
 
   list<string> elist;
   int num = formatConverter.supportedExtensions(elist);
