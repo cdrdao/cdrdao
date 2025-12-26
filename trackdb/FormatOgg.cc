c73806185c6fac4e8cf44b4b073e3ae6738cd37b
Michael Ortmann <ortmann@posteo.de>
2025-12-26 01:01:48 +0100

    Init ao_sample_format

--- trackdb/FormatOgg.cc
+++ trackdb/FormatOgg.cc
@@ -84,6 +84,7 @@ FormatSupport::Status FormatOgg::oggInit()
       return FS_WRONG_FORMAT;
   }
 
+  memset(&outFormat_, 0, sizeof(outFormat_));
   outFormat_.bits = 16;
   outFormat_.rate = 44100;
   outFormat_.channels = 2;
