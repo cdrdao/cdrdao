49448803f9b77b984f2e5c8a749858af467077d8
Michael Ortmann <ortmann@posteo.de>
2025-12-26 02:49:04 +0100

    Fix DragonFly port

--- dao/ScsiIf-freebsd-cam.cc
+++ dao/ScsiIf-freebsd-cam.cc
@@ -28,7 +28,11 @@
 #include <string.h>
 
 #include <camlib.h>
+#ifdef __DragonFly__
+#include <bus/cam/scsi/scsi_message.h>
+#else
 #include <cam/scsi/scsi_message.h>
+#endif
 
 #include "ScsiIf.h"
 #include "log.h"
