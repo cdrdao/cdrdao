0dc683ebccb295f69bbad7343c393e8ac450a6a5
Bryan Cain <bryancain3@gmail.com>
2026-01-26 20:49:41 -0700

    Don't refuse to burn discs with missing CD-Text fields
    Plenty of CDs in the real world have fields defined for some tracks but
    not others. cdrdao can make tocs from these discs without issue, and can encode the CD-Text just fine like this. So replace these errors with warnings that can be overridden with --force.
    
    Fixes cdrdao/cdrdao#72

--- trackdb/Toc.cc
+++ trackdb/Toc.cc
@@ -993,8 +993,8 @@ int Toc::checkCdTextData() const
                         "CD-TEXT: Language number %d: Language numbers are not continuously used.",
                         l);
 
-                if (err < 2)
-                    err = 2;
+                if (err < 1)
+                    err = 1;
             }
 
             last = l;
@@ -1046,8 +1046,8 @@ int Toc::checkCdTextData() const
         if (titleCnt > 0 && titleCnt != nofTracks_ + 1) {
             log_message(-2, "CD-TEXT: Language %d: %s field not defined for all tracks or disk.", l,
                         CdTextItem::packType2String(1, CdTextItem::PackType::TITLE));
-            if (err < 2)
-                err = 2;
+            if (err < 1)
+                err = 1;
         } else if (titleCnt == 0) {
             log_message(-1, "CD-TEXT: Language %d: %s field is not defined.", l,
                         CdTextItem::packType2String(1, CdTextItem::PackType::TITLE));
@@ -1058,8 +1058,8 @@ int Toc::checkCdTextData() const
         if (performerCnt > 0 && performerCnt != nofTracks_ + 1) {
             log_message(-2, "CD-TEXT: Language %d: %s field not defined for all tracks or disk.", l,
                         CdTextItem::packType2String(1, CdTextItem::PackType::PERFORMER));
-            if (err < 2)
-                err = 2;
+            if (err < 1)
+                err = 1;
         } else if (performerCnt == 0) {
             log_message(-1, "CD-TEXT: Language %d: %s field is not defined.", l,
                         CdTextItem::packType2String(1, CdTextItem::PackType::PERFORMER));
@@ -1070,37 +1070,37 @@ int Toc::checkCdTextData() const
         if (songwriterCnt > 0 && songwriterCnt != nofTracks_ + 1) {
             log_message(-2, "CD-TEXT: Language %d: %s field not defined for all tracks or disk.", l,
                         CdTextItem::packType2String(1, CdTextItem::PackType::SONGWRITER));
-            if (err < 2)
-                err = 2;
+            if (err < 1)
+                err = 1;
         }
 
         if (composerCnt > 0 && composerCnt != nofTracks_ + 1) {
             log_message(-2, "CD-TEXT: Language %d: %s field not defined for all tracks or disk.", l,
                         CdTextItem::packType2String(1, CdTextItem::PackType::COMPOSER));
-            if (err < 2)
-                err = 2;
+            if (err < 1)
+                err = 1;
         }
 
         if (arrangerCnt > 0 && arrangerCnt != nofTracks_ + 1) {
             log_message(-2, "CD-TEXT: Language %d: %s field not defined for all tracks or disk.", l,
                         CdTextItem::packType2String(1, CdTextItem::PackType::ARRANGER));
-            if (err < 2)
-                err = 2;
+            if (err < 1)
+                err = 1;
         }
 
         if (messageCnt > 0 && messageCnt != nofTracks_ + 1) {
             log_message(-2, "CD-TEXT: Language %d: %s field not defined for all tracks or disk.", l,
                         CdTextItem::packType2String(1, CdTextItem::PackType::MESSAGE));
-            if (err < 2)
-                err = 2;
+            if (err < 1)
+                err = 1;
         }
 
         if ((isrcCnt > 0 && isrcCnt != nofTracks_) ||
             (isrcCnt == 0 && cdtext_.getPack(l, CdTextItem::PackType::UPCEAN_ISRC) != NULL)) {
             log_message(-2, "CD-TEXT: Language %d: %s field not defined for all tracks.", l,
                         CdTextItem::packType2String(1, CdTextItem::PackType::UPCEAN_ISRC));
-            if (err < 2)
-                err = 2;
+            if (err < 1)
+                err = 1;
         }
     }
 
